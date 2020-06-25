7e183d8de29101c1d12032383437c0cb399b3db2
Florent Jaby <florent.jaby@gmail.com>
2020-06-25 17:58:54 +0200

    f

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const randomStart = Math.floor(Math.random() * 360)
 const debugElement = document.getElementById('debug')
+const compassElement = document.getElementById('compass')
+let attitude = randomStart
+
 function debug (obj) {
   debugElement.innerText = JSON.stringify(obj, null, '  ')
 }
+
 
 window.addEventListener("deviceorientation", onDeviceOrientation, true)
 
 function onDeviceOrientation (e) {
   console.log('e', e)
   const { alpha, beta, gamma } = e
-
+  debug({ alpha, beta, gamma })
+  attitude = randomStart + (alpha || 0)
+  debug({ attitude })
 }
