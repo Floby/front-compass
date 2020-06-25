3e10bbdf721574ab9eace71904065fe61787302f
Florent Jaby <florent.jaby@gmail.com>
2020-06-25 20:17:39 +0200

    f

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const randomStart = Math.floor(Math.random() * 360)
 const mainElement = document.querySelector('main')
-const compassElement = document.getElementById('compass')
 const northElement = document.getElementById('north')
 const needle = document.querySelector('#needle .needle')
-var attitude = randomStart
+var attitude = 0
 
 window.addEventListener("deviceorientation", onDeviceOrientation, true)
 
@@ -18,19 +16,10 @@ function onDeviceOrientation (e) {
 }
 
 function onAnimation () {
-  console.log('attitude', attitude)
-
   const decay = (180 - ((180+attitude)%360))
-  console.log('decay', decay)
-
   const RANGE = document.body.clientWidth * 2
-  console.log('RANGE', RANGE)
-
   const offset = decay * (RANGE / 180) * -1
   northElement.style.left = `${offset}px`
-
-  console.log('needle', needle)
-
   needle.style.transform = `rotateZ(${-decay}deg)`
 }
 
